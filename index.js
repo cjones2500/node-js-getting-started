b5244cd93ed406c3a99609cf89a1281ffa1d96a4
Christopher Jones <christopher.jones@physics.ox.ac.uk>
2015-01-13 23:25:53 +0000

    testing and playing around with sitatuion

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
 
+var http = require('http'),
+    fs = require('fs');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-	response.send(cool());
-   	//response.send('Hello World!');
+fs.readFile('./index.html', function (err, html) {
+
+	app.get('/', function(request, response) {
+		response.writeHeader(200,{"Content-Type": "text/html"});
+		response.write(html);
+		response.end();
+	});
+
 });
 
 var pg = require('pg');
