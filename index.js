0382aafa4fc44c0ab4159d299daeebbed2854337
Christopher Jones <christopher.jones@physics.ox.ac.uk>
2015-01-14 22:33:46 +0000

    added new data

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var path = require('path');
 
 var http = require('http'),
     fs = require('fs');
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(path.join(__dirname + 'public')));
 
-fs.readFile('./index.html', function (err, html) {
+//fs.readFile('./index.html', function (err, html) {
 
+	//route handler, express matches specific paths and completes them accordingly
 	app.get('/', function(request, response) {
-		response.writeHeader(200,{"Content-Type": "text/html"});
-		response.write(html);
-		response.end();
+		//an example of how to get information from within the headers
+		//console.log(request.headers['user-agent']);
+		console.log(request.headers);
+		
+		response.send('<html><body><h1>Hello World</h1></body></html>');
+		//response.writeHeader(200,{"Content-Type": "text/html"});
+		//response.write(html);
+		//response.end();
 	});
 
-});
+//});
 
-var pg = require('pg');
+/*var pg = require('pg');
 app.get('/db', function(request,response){
 	pg.connect(process.env.DATABASE_URL, function(err,client,done){
 		client.query('SELECT * FROM test_table',function(err,result){
@@ -27,9 +34,13 @@ app.get('/db', function(request,response){
 		else{ response.send(result.rows);}
 		});
 	});
+});*/
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
 });
 
 
-app.listen(app.get('port'), function() {
+/*app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-});
+});*/
